d2f464a80dc1e96d64a9974b95ece8af68e35c45
metaproject1805 <metataskinternational@gmail.com>
2024-11-13 02:27:35 +0100

    ref shit

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 // src/config.ts
 export const BASE_URL = "https://metatask-backend.ibgyzs.easypanel.host/api";
-export const FRONTBASE_URL = "https://metatask.international";
+export const FRONTBASE_URL = "https://metatask.ibgyzs.easypanel.host";
 export const WALLET_ID = "TUkVkxEcb4iLzJuHCFSvw77bSgKcBavTHp";
